3d059fb714fe40f61c2bc8ad61420b1cafdcd33a
kaczmarski-mk <kaczmarski.mk@gmail.com>
2025-06-03 16:44:22 +0200

    schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,23 @@
 
+CREATE TABLE Managers(
+PersonID int primary key auto_increment,
+LastName varchar(255),
+FirstName varchar(255),
+Address varchar(255),
+City varchar(255),
+Email VARCHAR(255),
+BirthDate DATE
+);
+
+CREATE TABLE Departments (
+DepartmentID INT PRIMARY KEY AUTO_INCREMENT
+,DepartmentName VARCHAR(100)
+,Location VARCHAR(100)
+,PersonID INT
+);
+
+CREATE TABLE Skills (
+SkillsID INT PRIMARY KEY AUTO_INCREMENT
+,SKillName VARCHAR(100)
+,PersonID INT
+);
